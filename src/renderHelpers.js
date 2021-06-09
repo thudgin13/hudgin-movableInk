895449241143e0d767f4b9e8c7a331ad384c6077
Traci <tas1013@gmail.com>
2021-06-09 00:33:23 -0500

    CSS: Add minimal CSS styling and flexbox display

--- src/renderHelpers.js
+++ src/renderHelpers.js
@@ -1,4 +1,7 @@
 import { convertDate } from "./utils";
+import cloudy from '../img/cloudy.png'
+import rain from '../img/rain.png'
+import sunny from '../img/sunny.png'
 
 const weatherContainer = document.getElementById('weatherContainer')
 const title = document.getElementById('dailyTitle')
@@ -11,18 +14,18 @@ export const renderWeather = (city, region, forecastArr) => {
     console.log("ForecastArr:", forecastArr)
     title.innerText = `Weather Forecast for ${city}, ${region}`
     const dailyForecastContainer = document.createElement('div')
+    dailyForecastContainer.setAttribute("id", "dailyForecast")
     weatherContainer.appendChild(dailyForecastContainer)
     let forecastHTML = ''
     forecastArr.map(day => {
         let dailyForecastHTML = `
-        <div>
-        <span>${day.icon}</span>
-        <p><strong>${Math.ceil(day.temperatureMax)}°</strong> / ${Math.floor(day.temperatureMin)}° F </p>
+        <div class='daily-weather'>
+        <img aria-label='cloudy' src=${day.icon}/>
+        <p>${day.icon}</p>
+        <p><strong>${Math.ceil(day.temperatureMax)}°</strong> / ${Math.floor(day.temperatureMin)}°F </p>
         </div>
         `
-        return forecastHTML += dailyForecastHTML
+        return forecastHTML += dailyForecastHTML += `<br>`
     })
     dailyForecastContainer.innerHTML = forecastHTML
-
-    console.log(dailyForecastContainer)
 }
